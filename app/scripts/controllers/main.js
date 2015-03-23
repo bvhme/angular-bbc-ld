2d6a6829f92a44d40f2b66b9be3647e07ff44fdf
Boris van Hoytema <i@bvh.me>
2015-03-23 20:24:48 +0000

    Updated controller

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularBbcLdApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularBbcLdApp
+ */
+angular.module('angularBbcLdApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
