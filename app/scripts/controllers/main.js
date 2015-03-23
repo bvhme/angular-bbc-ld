3d5f18d1d491e8234244d214dc0adfe08e94f720
Boris van Hoytema <i@bvh.me>
2015-03-23 14:21:26 +0000

    Test the Juicer3

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,7 +8,6 @@
  * Controller of the angularBbcLdApp
  */
 angular.module('angularBbcLdApp')
-  .controller('MainCtrl', ['$scope', 'juicer', 'things', '$log', function ($scope, juicer, things, $log) {
     .controller('MainCtrl', ['$scope', 'juicer', 'juicer3', 'things', '$log',
         function($scope, juicer, juicer3, things, $log) {
             $scope.responses = [];
@@ -24,9 +23,64 @@ angular.module('angularBbcLdApp')
             juicer.getArticles({
                 'query': 'Sun'
             }).then(success, fail);
+
             juicer.getArticle('18eb7370e4272dbfc410aea215d1e31bbcb24c1a').then(success, fail);
             juicer.getArticleByUrl('http://m.ewn.co.za/2015/03/22/Tunisian-president-says-third-suspect-in-museum-attack-on-the-run').then(success, fail);
+
             things.getById('e68c5b82-50e8-47d8-953c-bf61a2de0a85').then(success, fail);
             things.getByUri('http://www.bbc.co.uk/things/e68c5b82-50e8-47d8-953c-bf61a2de0a85').then(success, fail);
+
+            juicer3.getThing({
+                uri: 'http://dbpedia.org/resource/Barack_Obama',
+                limit: 5,
+            }).then(success, fail);
+            juicer3.findThingsMultiHop({
+                joinPredicate: 'http://purl.org/dc/terms/subject',
+                joinObject: 'http://dbpedia.org/resource/Category:United_Kingdom_Independence_Party_politicians',
+                limit: 200
+            }).then(success, fail);
+            juicer3.findCreativeWorks({
+                joinObject: 'http://dbpedia.org/resource/Category:United_Kingdom_Independence_Party_politicians',
+                limit: 200
+            }).then(success, fail);
+            juicer3.findCreativeWorksMultiHop({
+                aboutTagType: 'http://dbpedia.org/ontology/Person',
+                aboutTagPredicate: 'http://dbpedia.org/ontology/party',
+                aboutTagObject: 'http://dbpedia.org/resource/Conservative_Party_(UK)',
+                limit: 5,
+                afterDate: '2000-01-01'
+            }).then(success, fail);
+            juicer3.findCreativeWorksGeospatial({
+                point: '53.38,-1.47',
+                radius: '15km',
+                limit: 5
+            }).then(success, fail);
+            juicer3.findCreativeWorksGeoMultiHop({
+                aboutTagType: 'http://dbpedia.org/ontology/Person',
+                aboutTagPredicate: 'http://dbpedia.org/ontology/team',
+                aboutTagObject: 'http://dbpedia.org/resource/Manchester_United_F.C.',
+                point: '53.38,-1.47',
+                radius: '15km',
+                limit: 5
+            }).then(success, fail);
+            juicer3.findTaggedThingsMultiHop({
+                query: 'dav',
+                joinPredicate: 'http://dbpedia.org/ontology/party',
+                joinObject: 'http://dbpedia.org/resource/Conservative_Party_(UK)',
+                limit: 10
+            }).then(success, fail);
+            juicer3.findThingOccurrencesMultiHop({
+                type: 'http://dbpedia.org/ontology/Person',
+                joinPredicate: 'http://purl.org/dc/terms/subject',
+                joinObject: 'http://dbpedia.org/resource/Category:Conservative_Party_(UK)_MPs',
+                limit: 10
+            }).then(success, fail);
+            juicer3.findThingCoOccurrencesMultiHop({
+                type: 'http://dbpedia.org/ontology/Person',
+                joinPredicate: 'http://dbpedia.org/ontology/party',
+                joinObject: 'http://dbpedia.org/resource/Labour_Party_(UK)',
+                uri: 'http://dbpedia.org/resource/Len_McCluskey',
+                afterDate: '2015-01-01'
+            }).then(success, fail);
         }
     ]);
